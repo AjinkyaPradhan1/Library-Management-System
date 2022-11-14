2e87c3cdeeceb9c77ceb88160e54f80fcf587a9c
Ajinkya Pradhan <101694262+AjinkyaPradhan1@users.noreply.github.com>
2022-11-14 22:30:24 +0530

    Add files via upload

--- librarymanagment/librarymanagement/settings.py
+++ librarymanagment/librarymanagement/settings.py
@@ -133,5 +133,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sushilvishwakarma1002@gmail.com'
+EMAIL_HOST_USER = 'abc@gmail.com'
 EMAIL_HOST_PASSWORD = '1234'
